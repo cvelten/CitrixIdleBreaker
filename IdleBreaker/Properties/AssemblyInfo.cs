32284b00df4f3a0b6cb5b68839848d51185bf88a
cvelten <cv2415@columbia.edu>
2020-02-23 11:07:50 -0500

    updated assembly info

--- IdleBreaker/Properties/AssemblyInfo.cs
+++ IdleBreaker/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IdleBreaker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tool scanning for Citrix time out pop-ups and moving the mouse curser to break the screensaver timer.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Montefiore Medical Center - Radiation Oncology Physics Group")]
 [assembly: AssemblyProduct("IdleBreaker")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 (cvelten@montefiore.org)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
